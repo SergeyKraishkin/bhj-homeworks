107c3a4b2438d486cc28bc786e462bd58797bbb0
Сергей Краишкин <sergeykraishkin@Mac-mini-Sergej.local>
2023-11-26 20:43:04 +0300

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,10 @@
+let elem = document.getElementById('timer');
+
+const intervalId = setInterval(function() {
+    elem.innerText -= 1;
+    //debugger;
+    if (elem.innerText === "0") {
+        alert("Вы победили в конкурсе!");
+        clearInterval(intervalId);
+    }
+}, 1000);
